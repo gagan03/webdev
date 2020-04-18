32aee9db8e43a994c6bd653c743ae8a25e103c59
gagan03 <54581243+gagan03@users.noreply.github.com>
2020-04-18 20:40:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Netflix</title>
+</head>
+<body>
+<h1>Dark!!!</h1>
+<h2>A few facts about dark.The past,present and future.</h2>
+<p>A family saga with a supernatural twist, set in a German town, where the disappearance of two young children exposes the relationships among four families. </p>
+<img src="dark.jpg" height ="400" width="500">
+<iframe width="300" height="400" src="https://www.youtube.com/embed/BZ5OCIJVErw" frameborder="0" align="right" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p align="right">Click to watch the trailer of Dark.</p>
+<hr>
+<p><h3>About the Dark</h3></p>
+<ul>
+<li>Creators:Baran bo Odar, Jantje Friese</li>
+<li>Stars:<ins> Karoline Eichhorn, Louis Hofmann, Jördis Triebel </ins></li>
+</ul>
+<p></p>
+<p></p>
+<p><a href="https://en.wikipedia.org/wiki/Dark_(TV_series)">Click here to know more details</a></p>
+</body>
+</html>
